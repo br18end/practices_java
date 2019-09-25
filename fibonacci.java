1e74231b7a9bf4b7999a9df724714f5c3cf8ed26
br18end <43587217+br18end@users.noreply.github.com>
2019-09-24 20:24:01 -0700

    Initial commit

--- fibonacci.java
+++ fibonacci.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;	
 public class fibonacci {
-    int num;
-		int a = 1;
-
-		Scanner keyboard=new Scanner(System.in);
-		System.out.print("Enter number: ");
-		num=keyboard.nextInt();
-
-		for (int i=0; i<=num-1;) {
-			System.out.print(i+" ");
-			i=i+a;
-			a=i-a;
-		}
+	int num;
+	int a = 1;
+	
+	Scanner keyboard=new Scanner(System.in);
+	System.out.print("Enter number: ");
+	num=keyboard.nextInt();
+	
+	for (int i=0; i<=num-1;) {
+		System.out.print(i+" ");
+		i=i+a;
+		a=i-a;
+	}
 }
