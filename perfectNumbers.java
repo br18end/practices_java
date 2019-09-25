dbae0a46cbbff3a69d803882ec80afcd5862657b
br18end <43587217+br18end@users.noreply.github.com>
2019-09-24 20:20:35 -0700

    Initial commit

---
+++ perfectNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Practica43 {
+	public static void main(String[] args) {
+		int divisor = 1;
+		int num;
+		int perfect = 0;
+
+		Scanner keyboard=new Scanner(System.in);
+		System.out.print("Enter number: ");
+		num=keyboard.nextInt();
+
+		while(divisor < num){
+			if(num % divisor == 0){
+				perfect=perfect+divisor;
+			}
+			divisor=divisor+1;
+		}
+
+		if(perfect==num){
+			System.out.println("Perfect number");
+		}
+		else{
+			System.out.println("Not perfect number");
+		}
+	}
+}
